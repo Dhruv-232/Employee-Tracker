6f949e75fb40bc5d181eb759be105c73ce9d25de
Dhruv-232 <118102311+Dhruv-232@users.noreply.github.com>
2023-03-28 22:37:08 +1100

    Createed employee table

--- db/schema.sql
+++ db/schema.sql
@@ -17,3 +17,11 @@ CREATE TABLE role (
     REFERENCES department(id)
     ON DELETE CASCADE
 );
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+)
