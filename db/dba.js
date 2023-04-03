353d0292706c696ffa6c4ae8af40fa982b75e2e7
Dhruv-232 <118102311+Dhruv-232@users.noreply.github.com>
2023-04-03 22:26:41 +1000

    Made fetch all roles

--- db/dba.js
+++ db/dba.js
@@ -3,14 +3,19 @@ const connection = require("../connection");
 class DBA {
     // Keeping a reference to the connection on the class in case we need it later
     constructor(connection) {
-      this.connection = connection;
+        this.connection = connection;
     }
     fetchAllDepartments() {
         return this.connection.promise().query(
-          "SELECT department.id, department.name FROM department;"
+            "SELECT department.id, department.name FROM department;"
         );
-      }
-    
-  }
-  module.exports = new DBA(connection);
-  
+    }
+
+    fetchAllRoles() {
+        return this.connection.promise().query(
+            "SELECT role.id, role.title, role.salary, department.name as dept FROM role LEFT JOIN department on role.department_id=department(id);"
+        );
+    }
+
+}
+module.exports = new DBA(connection);
