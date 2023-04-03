0158e740a36d38b46e32415a83a06ea2070ce3a4
Dhruv-232 <118102311+Dhruv-232@users.noreply.github.com>
2023-04-03 22:06:59 +1000

    Made a new file dba and worked in it

--- db/seeds.sql
+++ db/seeds.sql
@@ -17,4 +17,13 @@ VALUES ("Boss", 150000, 1),
        ("Nurse", 60000, 4),
        ("Plumber", 73000, 5);
 
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Dhruv", "Sharma", 1, NULL),
+       ("Sam", "Bam", 2, 1),
+       ("Software Engineer", 60000, 3),
+       ("IT consultant", 55000, 3),
+       ("Head of IT", 90000, 3),
+       ("Doctor", 110000, 4),
+       ("Nurse", 60000, 4),
+       ("Plumber", 73000, 5);
        
