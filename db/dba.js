acbdb05705894899f5c75205dd05a40d90c92b12
Dhruv-232 <118102311+Dhruv-232@users.noreply.github.com>
2023-04-03 22:43:07 +1000

    Made changes to fetch all employees

--- db/dba.js
+++ db/dba.js
@@ -12,9 +12,14 @@ class DBA {
     }
 
     fetchAllRoles() {
-        console.log("Inside fetchAllRoles")
         return this.connection.promise().query(
             "SELECT role.id, role.title, role.salary, department.name AS dept FROM role LEFT JOIN department on role.department_id = department.id;"
+        );
+    }
+
+    fetchAllEmployees() {
+        return this.connection.promise().query(
+            "SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name AS dept CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id;"
         );
     }
 
